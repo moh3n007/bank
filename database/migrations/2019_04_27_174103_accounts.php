dcad976b8f2278d499bc931429817f3bc8fb7038
moh3n007 <lotfimohsen007@gmail.com>
2019-04-30 22:42:02 +0430

    create tables

--- database/migrations/2019_04_27_174103_accounts.php
+++ database/migrations/2019_04_27_174103_accounts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Accounts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('user-type')->default('admin');
-            $table->rememberToken();
+            $table->integer('aco-num');
+            $table->integer('user-id');
+            $table->integer('family-id');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('accounts');
     }
 }
--- database/migrations/2019_04_30_175938_payment-table.php
+++ database/migrations/2019_04_30_175938_payment-table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class PaymentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('payment', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('user-type')->default('admin');
-            $table->rememberToken();
+            $table->integer('loan-id');
+            $table->integer('pay-amount');
+            $table->date('last-payment');
+            $table->date('next-payment');
+            $table->integer('total-paid-amount');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('payment');
     }
 }
